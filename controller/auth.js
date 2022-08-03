ff85b489c14c8552dd5459af2135d3766eff0648
Adithyan Ashok <adithyanashok406@gmail.com>
2022-08-03 21:53:36 +0530

    cryptojs

--- controller/auth.js
+++ controller/auth.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose"
 import User from "../models/User.js"
-import bcrypt from "bcryptjs"
+import CryptoJs from "crypto-js"
 import { createError } from "../error.js";
 import  jwt  from "jsonwebtoken";
 
 
 export const signup = async (req, res, next) => {
+   const newUser =  new User({
+       name: req.body.name,
+       email: req.body.email,
+       password: CryptoJs.AES.encrypt(
+         req.body.password,
+         process.env.PASS_SEC
+       ).toString()
+      })
  try{
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const newUser = new User({ ...req.body, password: hash})
-    console.log(newUser);
-    await newUser.save();
-    res.status(201).send("userCreated")
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+    console.log(savedUser)
  } catch (err) {
     next(err)
  }
@@ -23,20 +28,23 @@ export const signin = async (req, res, next) => {
      const user = await User.findOne({ name: req.body.name });
      if (!user) return next(createError(404, "User not found!"));
  
-     const isCorrect = await bcrypt.compare(req.body.password, user.password);
- 
-     if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
- 
-     const token = jwt.sign({ id: user._id }, process.env.JWT);
-     const { password, ...others } = user._doc;
- 
-     res.cookie("access_token",token,{
-         httpOnly: true,
-         secure: true,
-       })
-       .status(200)
-       .json(others);
+     const hashedPassword = CryptoJs.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+     )
+      const OriginalPassword = hashedPassword.toString(CryptoJs.enc.Utf8)
+      OriginalPassword !== req.body.password && res.status(401).json("Wrong Password")
+       const accessToken = jwt.sign(
+         {
+            id: user._id,
+         },
+         process.env.JWT,
+         { expiresIn:"9d" }
+       )
+       const {password, ...others} = user._doc;
+       res.status(200).json({...others, accessToken})
    } catch (err) {
+      console.log(err)
      next(err);
    }
  };
