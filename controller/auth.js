e47aa8cb05b8493102ee9d21babff3468bf7b6d0
Adithyan Ashok <adithyanashok406@gmail.com>
2022-10-15 17:10:04 +0530

    sample

--- controller/auth.js
+++ controller/auth.js
@@ -11,16 +11,22 @@ export const signup = async (req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({ ...req.body, password: hash})
     console.log(newUser);
-    await newUser.save();
-    res.status(200).send("userCreated")
- } catch (err) {
-    next(err)
- }
+    const savedUser = await newUser.save();
+    const token = jwt.sign({id :savedUser._id}, process.env.JWT);
+       const { password, ...others } = savedUser._doc
+ 
+     res.cookie("access_token",token,{ 
+        token:token
+       }).status(200).json(others);
+   } catch (err) {
+     next(err);
+   }
+ 
 }
 
 export const signin = async (req, res, next) => {
    try {
-     const user = await User.findOne({ name: req.body.name });
+     const user = await User.findOne({ email: req.body.email });
      if (!user) return next(createError(404, "User not found!"));
  
       const isCorrect = await bcrypt.compare(req.body.password, user.password);
